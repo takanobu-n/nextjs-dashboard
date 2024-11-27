75fdb726c60cea0a42a42186c7987b3a5439a37a
takanobu <takanobu.n.22@gmail.com>
2024-11-28 00:18:19 +0900

    deployするために改良

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // その他の設定がある場合はここに記述
-
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000, // 1秒ごとにファイル変更を確認
-      aggregateTimeout: 300, // 変更後の待機時間
-    };
-    return config;
-  },
+  // 必要な他の設定があればここに記述
+  reactStrictMode: true, // ReactのStrictモードを有効にする例
+  swcMinify: true, // SWCを使った高速なMinify
 };
 
-module.exports = nextConfig;
+export default nextConfig;
